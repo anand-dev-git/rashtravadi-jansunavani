30992736c5f3534304dda851965ab1730ada78c4
Anand <67869656+anand-dev-git@users.noreply.github.com>
2025-09-09 10:41:31 +0530

    Update page.tsx

--- app/update/page.tsx
+++ app/update/page.tsx
@@ -207,7 +207,7 @@ function UpdatePageContent() {
                       <option value="Problem Solved">Problem Solved</option>
                     </select>
                   </div>
-                  <div>
+{/*                   <div>
                     <label
                       htmlFor="member"
                       className="block text-sm font-medium text-gray-700"
@@ -222,7 +222,7 @@ function UpdatePageContent() {
                       onChange={(e) => setMember(e.target.value)}
                       className="mt-1 block w-full rounded-md border border-gray-300 bg-white px-3 py-2 text-gray-900 shadow-sm placeholder:text-gray-400 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                     />
-                  </div>
+                  </div> */}
                 </div>
                 <div>
                   <label
